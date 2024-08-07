654e44ce6895de5786c328b3352204c7d45aaa9c
Ashish singh <ashishsingh1331@gmail.com>
2024-08-07 15:49:32 +0530

    Feature of Image component of nextjs, fill, blur, priority, optimized webp automatically.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { inter, roboto } from "./font";
+import Aashi from "../img/aashi.jpg";
 
 export default function Home() {
   return (
     <>
+      <div style={{ width: "500px", height: "500px", position: "relative" }}>
+        <Image
+          priority
+          placeholder="blur"
+          fill
+          src={Aashi}
+          alt="Daugher image"
+        />
+      </div>
       <h1 className={inter.className}>Home</h1>
       <p className={roboto.className}>Paragraph</p>
     </>
